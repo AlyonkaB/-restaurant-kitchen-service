0f3ea93d67cb2d67d7a109ed641a0d13096d14d6
Alona <alyonkabagritskaya@gmail.com>
2024-01-19 18:29:00 +0200

    add models dish, dishtype, cook & make first migration

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_for_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return (f"{self.username}: {self.first_name} {self.last_name} "
+                f"({self.years_for_experience} years for experience)")
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cook = models.ManyToManyField(Cook, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} {self.price}"
