a67778c03b303ff7d716b72854df10b92630c530
Alona <alyonkabagritskaya@gmail.com>
2024-01-20 10:11:07 +0200

    add model Ingridient, corrected model Dish

--- kitchen/models.py
+++ kitchen/models.py
@@ -24,12 +24,23 @@ class Cook(AbstractUser):
                 f"({self.years_for_experience} years for experience)")
 
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
 class Dish(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
     price = models.DecimalField(decimal_places=2, max_digits=10)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
-    cook = models.ManyToManyField(Cook, related_name="dishes")
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
 
     class Meta:
         ordering = ["name"]
