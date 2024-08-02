5c06727bb6d92492237e69fd4609f84d02f7b230
AlyonkaB <alyonkabagritskaya@gmail.com>
2024-08-02 10:11:45 +0300

    update readme.md file

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "restaurant_kitchen_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates'],
+        "DIRS": [BASE_DIR / 'img/../templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
