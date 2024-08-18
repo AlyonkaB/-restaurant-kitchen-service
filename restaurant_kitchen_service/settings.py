2787b71a4d38953042c11f6bee29e7456c24d732
AlyonkaB <alyonkabagritskaya@gmail.com>
2024-08-18 17:54:02 +0300

    update settings & requirements.txt

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vy!#sv5mlv=@n$=49aq92q2-av^*$p2qc^3sp%wxt!irx6&w$0"
-
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-vy!#sv5mlv=@n$=49aq92q2-av^*$p2qc^3sp%wxt!irx6&w$0")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
@@ -132,4 +131,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "kitchen.Cook"
 
-LOGIN_REDIRECT_URL = "/kitchen/"
+LOGIN_REDIRECT_URL = "/"
+
--- restaurant_kitchen_service/urls.py
+++ restaurant_kitchen_service/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("kitchen/", include("kitchen.urls", namespace="kitchen")),
+    path("", include("kitchen.urls", namespace="kitchen")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls"))
 ]
