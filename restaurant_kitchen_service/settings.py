9f1b92f4b250929b66876208c9519820365cb585
AlyonkaB <alyonkabagritskaya@gmail.com>
2024-08-21 21:25:11 +0300

    ctaticfiles managing

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-vy!#sv5mlv=@n$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
+
 ALLOWED_HOSTS = ["localhost"]
 
 INTERNAL_IPS = [
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -124,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = "staticfiles/"
+
 STATICFILES_DIRS = [
     BASE_DIR/"static",
 ]
